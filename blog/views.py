76a23b02e50d08bb6a6dc12ccaaad45e518db3ad
HafsaA85 <hafsaadeel4@gmail.com>
2025-09-11 13:56:18 +0100

    third commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render # pyright: ignore[reportMissingModuleSource]
+from django.http import HttpResponse # pyright: ignore[reportMissingModuleSource]
 
 # Create your views here.
 def index(request):
